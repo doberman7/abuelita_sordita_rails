f8e2b8d80f82af5cb1d75926bf2a62c2a50817e8
Julio <juliodevasquez@gmail.com>
2017-07-18 22:01:55 -0500

    render funcionales

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,4 +1,7 @@
 class FrontController < ApplicationController
   def index
   end
+  def create
+    p @new_message = params[:new_message]    
+  end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,21 @@
 class MessagesController < ApplicationController
-  
+  def index
+    render 'front/index' 
+  end
+  def create
+    @new_message = params[:new_message]        
+
+    case @new_message
+      when 'bye tqm' 
+         @new_message = 'adios nieto'
+         render 'front/index' 
+      when @new_message.upcase   
+         @new_message ="NO, NO DESDE 1983"
+         render 'front/index' 
+      else render 'messages/new'
+    end  
+
+    
+
+  end
 end
